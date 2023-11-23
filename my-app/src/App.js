69f8e3d668477c8eb3c410900aded72730e3e181
Anupriya408 <pihusinha0408@gmail.com>
2023-11-23 21:07:09 +0530

    half

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,23 +1,101 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
+import './Todo.css';
+
+const API_ENDPOINT = 'https://jsonplaceholder.typicode.com/users/1/todos';
 
 function App() {
+  const [todos, setTodos] = useState([]);
+  const [newTask, setNewTask] = useState('');
+  const [showCompleted, setShowCompleted] = useState(false);
+
+  useEffect(() => {
+    // Fetch initial todos from the API 
+    fetch(API_ENDPOINT)
+      .then((response) => response.json())
+      .then((data) => setTodos(data))
+      .catch((error) => console.error('Error fetching todos:', error));
+  }, []);
+
+  const handleAddTask = () => {
+    if (newTask.trim() === '') {
+      alert('Task name cannot be empty!');
+      return;
+    }
+
+    const newTodo = {
+      userId: 1,
+      id: todos.length + 1,
+      title: newTask,
+      completed: false,
+    };
+
+    setTodos([...todos, newTodo]);
+    setNewTask('');
+  };
+
+  const handleToggleCompletion = (id) => {
+    setTodos((prevTodos) =>
+      prevTodos.map((todo) =>
+        todo.id === id ? { ...todo, completed: !todo.completed } : todo
+      )
+    );
+  };
+
+  const handleEditTask = (id, newTitle) => {
+    setTodos((prevTodos) =>
+      prevTodos.map((todo) =>
+        todo.id === id ? { ...todo, title: newTitle } : todo
+      )
+    );
+  };
+
+  const handleDeleteTask = (id) => {
+    setTodos((prevTodos) => prevTodos.filter((todo) => todo.id !== id));
+  };
+
+  const filteredTodos = showCompleted
+    ? todos.filter((todo) => todo.completed)
+    : todos;
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React TODO
-        </a>
-      </header>
+      <h1>TODO APP</h1>
+      <div>
+        <input
+          type="text"
+          placeholder="Enter New Task"
+          value={newTask}
+          onChange={(e) => setNewTask(e.target.value)}
+        />
+        <button onClick={handleAddTask}>Add New Task</button>
+      </div>
+      <div>
+        <label>
+          <input
+            type="checkbox"
+            checked={showCompleted}
+            onChange={() => setShowCompleted(!showCompleted)}
+          />
+          Show All Task Completed
+        </label>
+      </div>
+      <ul>
+        {filteredTodos.map((todo) => (
+          <li key={todo.id}>
+            <span
+              // style={{ textDecoration: todo.completed ? 'line-through' : 'none' }}
+              className={todo.completed ? 'completed' : '' }
+              onClick={() => handleToggleCompletion(todo.id)}
+            >
+              {todo.title}
+            </span>
+            <button onClick={() => handleEditTask(todo.id, prompt('Edit the task:', todo.title))}>
+              Edit Task
+            </button>
+            <button onClick={() => handleDeleteTask(todo.id)}>Delete Task</button>
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
